ea68f9bdf3bbbe11dc5f781998c3c3c6d69954c5
Mirza Krupic <krupicmirza99@gmail.com>
2021-07-07 21:12:07 +0200

    add middleware and post layout

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,15 @@ use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function(){
     return view('home');
 })->name('home');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+->name('dashboard')
+->middleware('auth');
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
@@ -20,6 +23,4 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
