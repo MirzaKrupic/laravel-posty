f2e75a30de54ac2682c499ca11c17b8713c96735
Mirza Krupic <krupicmirza99@gmail.com>
2021-07-10 17:36:34 +0200

    added security to post form

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth'])->only(['store', 'destroy']);
+    }
+
     public function index()
     {
         $posts = Post::with(['user', 'likes'])->orderBy('created_at', 'desc')->paginate(20);
